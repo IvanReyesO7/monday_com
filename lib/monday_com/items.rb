db38a9e0baf567c82bd5be2e4348092cac5f34d0
Ivan Reyes <ivanwilf@hotmail.com>
2021-08-29 15:54:22 +0900

    Ready client with board and item instances

--- lib/monday_com/items.rb
+++ lib/monday_com/items.rb
@@ -4,11 +4,27 @@ class Item
   def initialize(item)
     @id = item["id"]
     @name = item["name"]
-    @status = status(item)
+    @status = decode_status(status(item))
   end
 
   def status(item)
-    item["column_values"].select { |item| item["title"] == "Status"}
+    status_hash = item["column_values"].select { |item| item["title"] == "Status" }[0]["value"]
+    status_hash.nil? ? ":5" : status_hash.match(/index"(:\d)/)[1]
   end
 
+  def decode_status(status_code)
+    stauts_table = {
+      ":0": "Working on it",
+      ":1": "Done",
+      ":2": "Stuck",
+      ":3": "Live in Production",
+      ":4": "Await Response",
+      ":5": "Not Started",
+      ":6": "Pull Request",
+      ":7": "Ready for Code Review",
+      ":9": "On Hold - Client request",
+      ":10": "Testing"
+    }
+    stauts_table[status_code.to_sym]
+  end
 end
